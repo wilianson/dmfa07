23d0e4410463c6e60f55c5193c78054060dd4484
christangrant <christangrant@556b7404-d43e-0410-9d03-fbcebc09fa54>
2007-11-22 20:19:51 +0000

    Netbeans project was started, for kmeans algorithm

---
+++ trunk/kmeans/kmeans/src/kmeans/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Main.java
+ * 
+ * Created on Nov 22, 2007, 3:16:34 PM
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kmeans;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
