9ce932cb7dd93d642f0d280ee348da5e8de93418
christangrant <christangrant@556b7404-d43e-0410-9d03-fbcebc09fa54>
2007-11-23 04:39:36 +0000

    Added KMeans class

---
+++ kmeans/kmeans/src/kmeans/KMeans.java
@@ -0,0 +1,23 @@
+/*
+ * KMeans.java
+ * 
+ * Created on Nov 22, 2007, 11:37:35 PM
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kmeans;
+
+/**
+ *
+ * @author Administrator
+ */
+public class KMeans {
+
+    private DataSet dataset;
+
+    public KMeans() {
+    }
+    
+}
